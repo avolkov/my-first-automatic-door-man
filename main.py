5b2a6d3765fff711c22a1d76b516bc2e9558a0c1
Alex Volkov <alex@flamy.ca>
2015-12-06 17:18:56 -0500

    Fixed all PEP8 errors.

--- main.py
+++ main.py
@@ -8,22 +8,31 @@ import random
 
 
 BLUR = 15
-knownPeople = {'42:4B:F9:AB:0D:5C' : ("Thiago", "Old sport")}
-presetMessages = ["Nice weather today isn't it,", "Nice shirt,", "Have a wonderful day,", "Nice to see you here,", "I'm running out of greetings again",
-                  "I'm not being paid enough for this", "Good morning. No.   Wait. Good afternoon. No.   Wait. Good morning. No.    Wait. Nevermind", "I think I've saw you before"]
+knownPeople = {'42:4B:F9:AB:0D:5C': ("Thiago", "Old sport")}
+presetMessages = [
+    "Nice weather today isn't it,",
+    "Nice shirt,",
+    "Have a wonderful day,",
+    "Nice to see you here,",
+    "I'm running out of greetings again",
+    "I'm not being paid enough for this",
+    "Good morning. No.   Wait. Good afternoon. No.   \
+Wait. Good morning. No. Wait. Nevermind",
+    "I think I've saw you before"]
+
 
 def getAdjustedThreshhold(cap):
 
     ret, base = cap.read()
-    base = cv2.blur(base,(BLUR,BLUR))
-    base = cv2.cvtColor(base,cv2.COLOR_BGR2GRAY)
+    base = cv2.blur(base, (BLUR, BLUR))
+    base = cv2.cvtColor(base, cv2.COLOR_BGR2GRAY)
 
     for i in range(0, 60):
 
         ret, frame = cap.read()
 
-        frame = cv2.blur(frame,(BLUR,BLUR))
-        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        frame = cv2.blur(frame, (BLUR, BLUR))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         diff = cv2.absdiff(frame, base)
         ret, diff = cv2.threshold(diff, i, 255, cv2.THRESH_BINARY)
@@ -33,13 +42,14 @@ def getAdjustedThreshhold(cap):
 
     return i
 
+
 def getGreetingMessage(callingName):
 
     message = ''
 
-    rand = int (random.random() * 2)
+    rand = int(random.random() * 2)
 
-    if rand == 0: # time based greeting
+    if rand == 0:  # time based greeting
         currentHour = datetime.now().hour
         if currentHour >= 5 and currentHour <= 11:
             message += "Good morning,"
@@ -49,9 +59,8 @@ def getGreetingMessage(callingName):
             message += "Good evening,"
         else:
             message += "Good night,"
-    else: # vague greeting
-        message += presetMessages[ (int) (random.random() * len(presetMessages)) ]
-
+    else:  # vague greeting
+        message += presetMessages[(int)(random.random() * len(presetMessages))]
 
     if callingName is not None:
         message += " " + callingName
@@ -69,8 +78,8 @@ print "Threshold: " + str(threshold)
 
 print "Starting!"
 ret, base = cap.read()
-base = cv2.blur(base, (BLUR,BLUR))
-base = cv2.cvtColor(base,cv2.COLOR_BGR2GRAY)
+base = cv2.blur(base, (BLUR, BLUR))
+base = cv2.cvtColor(base, cv2.COLOR_BGR2GRAY)
 
 isSensorActivated = False
 wasSensorActivated = False
@@ -79,16 +88,18 @@ while True:
 
     ret, frame = cap.read()
 
-    frame = cv2.blur(frame, (BLUR,BLUR))
+    frame = cv2.blur(frame, (BLUR, BLUR))
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     diffFrame = cv2.absdiff(frame, base)
 
-    ret, diffFrame = cv2.threshold(diffFrame, threshold, 255, cv2.THRESH_BINARY)
+    ret, diffFrame = cv2.threshold(
+        diffFrame, threshold, 255, cv2.THRESH_BINARY)
 
     height, width = diffFrame.shape[:2]
 
-    sensorFrame = diffFrame[(height/5)*2:(height/5)*3, (width/5)*2:(width/5)*3]
+    sensorFrame = diffFrame[
+        (height / 5) * 2:(height / 5) * 3, (width / 5) * 2:(width / 5) * 3]
 
     frameHeight, frameWidth = sensorFrame.shape[:2]
     sensorFramePixels = frameHeight * frameWidth
@@ -104,9 +115,10 @@ while True:
         message = ''
 
         for mac in knownPeople:
-            lastTime = bluetoothSurveyor.getDeviceLastSeenAgeInSecondsByMAC(mac)
+            lastTime = bluetoothSurveyor.getDeviceLastSeenAgeInSecondsByMAC(
+                mac)
 
-            if lastTime is not None and lastTime < 180: # known person found
+            if lastTime is not None and lastTime < 180:  # known person found
                 message = getGreetingMessage(knownPeople[mac][1])
                 greeted = True
                 break
@@ -117,13 +129,15 @@ while True:
             del knownPeople[mac]
 
         process1 = subprocess.Popen(["echo", message], stdout=subprocess.PIPE)
-        process2 = subprocess.Popen(["text2wave"], stdin=process1.stdout, stdout=subprocess.PIPE)
-        subprocess.Popen(["play", "-t", "wav", "-", "tempo", str(0.8), "pitch", str(-100)], stdin=process2.stdout)
-
+        process2 = subprocess.Popen(
+            ["text2wave"], stdin=process1.stdout, stdout=subprocess.PIPE)
+        subprocess.Popen(["play", "-t", "wav", "-", "tempo",
+                          str(0.8), "pitch", str(-100)], stdin=process2.stdout)
 
     wasSensorActivated = isSensorActivated
 
-    cv2.rectangle(diffFrame, ((width/5)*2,(height/5)*2), ((width/5)*3,(height/5)*3), (255, 255, 255), 1)
+    cv2.rectangle(diffFrame, ((width / 5) * 2, (height / 5) * 2),
+                  ((width / 5) * 3, (height / 5) * 3), (255, 255, 255), 1)
     cv2.imshow('diff', diffFrame)
 
     k = cv2.waitKey(30) & 0xff
