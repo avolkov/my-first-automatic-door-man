f9bf52a14e2985a422833bdbb5b970554eaa4820
Alex Volkov <alex@flamy.ca>
2016-01-02 18:51:13 -0500

    Adding adjust_`threshold class

--- picam_loop.py
+++ picam_loop.py
@@ -3,6 +3,71 @@ from picamera import PiCamera
 #import time
 import cv2
 
+BLUR = (15, 15)
+
+
+def blur_gray(in_frame):
+    out_frame = cv2.blur(in_frame, BLUR)
+    out_frame = cv2.cvtColor(out_frame, cv2.COLOR_BGR2GRAY)
+    return out_frame
+
+
+class adjust_threshold(object):
+    """Analyse several initial frames and adjust threshold"""
+
+    THRESHOLD_ADJUST_MAX = 60
+
+    def __init__(self):
+        self.counter = 0
+        self.baseframe = None
+        self._adjusted = False
+        self._threshold = 0
+
+    def run_adjustment(self, image):
+        """
+        Run threshold adjustment for THRESHOLD_ADJUST_MAX cycles.
+        Here be dragons.
+        """
+        if not self.baseframe:
+            self.baseframe = blur_gray(image)
+            return
+
+        frame = blur_gray(image)
+        diff = cv2.absdiff(frame, self.baseframe)
+        diff = cv2.threshold(diff, self.counter, 255, cv2.THRESH_BINARY)
+        if cv2.countNonZero(diff) == 0:
+            self._threshold = self.counter + 5
+            self.adjustement_complete = True
+            return
+
+        if self.counter == self.THRESHOLD_ADJUST_MAX:
+            self._threshold = self.counter
+            self._adjusted = True
+
+        self.counter += 1
+
+    @property
+    def adjusted(self):
+        return self._adjusted
+
+    @property
+    def threshold(self):
+        return self._threshold
+
+
+class doorman(object):
+
+    def __init__(self):
+        pass
+
+    def doorman_detect(self, frame):
+        frame = cv2.blur(frame, BLUR)
+        # TODO conver to gray with picam
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    def process_frame(self, image):
+        pass
+
 
 def init_camera(resolution=(640, 480), framerate=32):
     """
